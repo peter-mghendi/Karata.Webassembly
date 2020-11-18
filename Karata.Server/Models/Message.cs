3f1d0b4cf3e5024b68834e97501ca1c55efad2d7
sixpeteunder <plenjo15@gmail.com>
2020-11-18 10:46:06 +0300

    Add project files.

---
+++ Karata.Server/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Karata.Server.Models
+{
+    public abstract class Message<T>
+    {
+        public string Content { get; set; }
+    }
+}
